e16606ca4955deeb5cfd8a407b07063787198a6f
sugrivnandewad <sugrivnandewad@gmail.com>
2025-06-02 21:15:02 +0530

    Create main.tf

---
+++ EC2/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "network" {
+    source = "../aws_network"
+  
+}
+
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+resource "aws_instance" "dev_instance" {
+  ami           = data.aws_ami.latest_amazon_linux.id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.my_key_pair.key_name
+  subnet_id = module.network.public_subnet_ids[0]
+  tags = {
+    Name = "DevInstance"
+    Owner = "DevTeam"
+    Environment = "Development"
+    Project = "TerraformDemo"
+    Role = "WebServer"
+  }
+}
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my_key_pair"
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
+resource "local_file" "private_key_pem" {
+  filename        = "${aws_key_pair.my_key_pair.key_name}.pem"
+  content         = tls_private_key.private_key.private_key_pem
+  file_permission = "0400"
+}
