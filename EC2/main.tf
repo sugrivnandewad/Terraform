a7b917b55dcc501bf370a6225ebf34ad6f3d3821
sugrivnandewad <sugrivnandewad@gmail.com>
2025-06-02 23:12:11 +0530

    Update main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -20,10 +20,29 @@ data "terraform_remote_state" "network" {
   }
 
 }
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
 resource "aws_instance" "dev_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Example AMI, replace with a valid one
+  ami           = data.aws_ami.latest_amazon_linux.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.my_key_pair.key_name
+  subnet_id = module.network.public_subnet_ids[0]
+  tags = {
+    Name = "DevInstance"
+    Owner = "DevTeam"
+    Environment = "Development"
+    Project = "TerraformDemo"
+    Role = "WebServer"
+  }
 }
 
 resource "tls_private_key" "private_key" {
