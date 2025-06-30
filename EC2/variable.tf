24f98df285ab052ea2f873ce5e200ec64f545cea
sugrivnandewad <sugrivnandewad@gmail.com>
2025-06-30 12:47:58 +0530

    Update variable.tf

--- EC2/variable.tf
+++ EC2/variable.tf
@@ -4,3 +4,8 @@ variable "aws_region" {
   default     = "ap-south-1"
 
 }
+variable "aws_security_IP" {
+  description = "The IP address to allow SSH access to the EC2 instances"
+  type        = string
+  default     = "0.0.0/0" # Change this to your specific IP address or CIDR block for security purposes
+}
