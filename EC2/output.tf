b071ac8e985040dbcd579482f3cbb2bf49f9bdc4
sugrivnandewad <sugrivnandewad@gmail.com>
2025-06-30 12:47:35 +0530

    Update output.tf

--- EC2/output.tf
+++ EC2/output.tf
@@ -1,1 +1,9 @@
+output "ec2_instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.dev_instance.id
+}
 
+output "ec2_private_instance_id" {
+  description = "The ID of the private EC2 instance"
+  value       = aws_instance.dev_private_instance.id
+}
